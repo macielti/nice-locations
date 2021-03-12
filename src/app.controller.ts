3a782c31512cb4486d3629b32a5baabe9980c0a5
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2021-03-12 15:31:39 -0300

    fix e2e tests

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
+import { resolve } from 'path';
+
+interface ILocationListDto {
+  locations: string[];
+}
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get('')
-  getHello(): string {
-    return this.appService.getHello();
+  @Render('list.hbs')
+  listLocations(): ILocationListDto {
+    return {
+      locations: ['Location 1', 'Location 2', 'Location 3'],
+    };
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { resolve } from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const APP_DIRECTORY = resolve(__dirname, '..');
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.setViewEngine('hbs');
+  app.setBaseViewsDir(resolve(APP_DIRECTORY, 'views'));
+  app.useStaticAssets(resolve(APP_DIRECTORY, 'public'));
+
   await app.listen(3000);
 }
 bootstrap();
