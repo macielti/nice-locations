84044c33079e0b28671726557e412c3db4538974
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2021-03-15 10:41:47 -0300

    creating a location entity

--- src/location/location.module.ts
+++ src/location/location.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { LocationController } from './location.controller';
+import { Location } from './location.entity';
 import { LocationService } from './location.service';
 
 @Module({
   controllers: [LocationController],
+  imports: [TypeOrmModule.forFeature([Location])],
   providers: [LocationService],
 })
 export class LocationModule {}
