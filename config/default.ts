c0b675126d3b87c979091ab88a1e9477a067b329
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2021-03-15 10:28:56 -0300

    rest of potgresql connection

--- config/default.ts
+++ config/default.ts
@@ -1,10 +1,20 @@
 import { resolve } from 'path';
 
+const SOURCE_PATH = resolve(__dirname, '..', 'src');
+
 export default {
-  templates: {
-    path: resolve(__dirname, '..', 'views'),
+  database: {
+    url:
+      'postgres://NICE_LOCATIONS_NODE_USER:NICE_LOCATIONS_NODE_PASSWORD@localhost:6304/NICE_LOCATIONS_NODE_DATABASE',
+  },
+  orm: {
+    entities: [`${SOURCE_PATH}/**/*.entity.ts`],
+    synchronize: false,
   },
   public: {
     path: resolve(__dirname, '..', 'public'),
+  },
+  templates: {
+    path: resolve(__dirname, '..', 'views'),
   },
 };
